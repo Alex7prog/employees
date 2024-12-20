50965c71b5b5e4df53d8df36b834f59890fd3fa3
Alex Sedmoy <alx7.prog@gmail.com>
2024-12-20 19:16:10 +0200

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
-import Employees from './features/Employees';
-
-import store from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
+import store from './redux/store';
+
+import Employees from './features/Employees';
 
 const App: FC = () => {
   return (
